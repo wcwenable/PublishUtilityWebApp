fd7b57982858afb9cd598335e0d311338ad9ef74
王成伍 <wcw515@163.com>
2018-10-09 23:20:21 +0800

    add jquery search logic

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -18,8 +19,35 @@ namespace PublishUtilityWebApp.Controllers
 
         public IActionResult ProcessForPublish(string sourceDir)
         {
-            ViewBag.sourceDir = sourceDir;  
+            string msg = string.Empty;//默认成功
+            msg = CheckSourceDirExists(sourceDir, msg);
+            if (string.IsNullOrWhiteSpace(msg))//源目录存在
+            {
+                ViewBag.sourceDir = sourceDir;   
+            }
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult DoSearchForPublish(string sourceDir,string searchKey)
+        {
+            string msg = string.Empty;//默认成功
+            msg = CheckSourceDirExists(sourceDir, msg);
+            if (string.IsNullOrWhiteSpace(msg))//源目录存在
+            {
+                //todo:在源目录中查找所有这样的文件（需要包含文件目录）：其文件名中含有搜索关键字
+            }
+            return Content(msg);
+        }
+
+        private static string CheckSourceDirExists(string sourceDir, string msg)
+        {
+            if (!Directory.Exists(sourceDir))
+            {
+                msg = "源目录不存在";
+            }
+
+            return msg;
         }
 
         public IActionResult About()
